00b5e21748077e5c63af1cb9c43d695a4df33172
iraap11 <irapansare@gmail.com>
2025-11-15 13:46:40 +0530

    Reverse string program using char{} and  Array.Reverse()

---
+++ ReverseString/UsingCharAndArrayReverse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_Basic_Programs.ReverseString
+{
+    internal class UsingCharAndArrayReverse
+    {
+        static void Main()
+        {
+            Console.Write("Enter your string2 : ");
+            string str = Console.ReadLine();
+
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+
+            string reversed = new string(arr);
+            Console.WriteLine(reversed);
+
+            if (str.ToLower() == reversed.ToLower())
+            {
+                Console.WriteLine("Is Palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("Is not Palindrome");
+            }
+        }
+    }
+}
