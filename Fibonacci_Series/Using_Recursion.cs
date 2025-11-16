5cc4b6c9fced5cd85aa2ed1de993f6ad7100528b
iraap11 <irapansare@gmail.com>
2025-11-17 00:35:28 +0530

    Added Fibonacci series using recursion

---
+++ Fibonacci_Series/Using_Recursion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace All_Basic_Programs.Fibonacci_Series
+{
+    internal class Using_Recursion
+    {
+        static int Fib(int n)
+        {
+
+            if (n <= 1)
+                return n;
+            return Fib(n - 1) + Fib(n - 2);
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter the number : ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Fibonacci Series : ");
+
+            for (int i = 0; i <= n; i++)
+            {
+                Console.Write(Fib(i) + " ");
+            }
+        }
+
+    }
+}
