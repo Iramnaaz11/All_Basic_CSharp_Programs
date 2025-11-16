ee8a876164d53fdbc3c58ad8a87992ec3d12c93c
iraap11 <irapansare@gmail.com>
2025-11-16 22:44:38 +0530

    Added a fibonacci series program using for loop

---
+++ Fibonacci_Series/Using_For_Loop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+
+namespace All_Basic_Programs.Fibonacci_Series
+{
+    internal class Using_For_Loop
+    {
+        static void Main()
+        {
+            Console.Write("Enter the count : ");
+            int n = int.Parse(Console.ReadLine());
+
+            int a = 0, b = 1, c;
+
+            Console.Write("Fibonacci series :");
+
+            for (int i = 2; i <= n; i++)
+            {
+                Console.Write(a + " ");
+
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
+
