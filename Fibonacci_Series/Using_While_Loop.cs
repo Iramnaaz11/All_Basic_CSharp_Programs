32aa5c5450ea6f8b9f497526270fabba7047b18b
iraap11 <irapansare@gmail.com>
2025-11-16 22:56:02 +0530

    Added fibonacci series program using while loop

---
+++ Fibonacci_Series/Using_While_Loop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace All_Basic_Programs.Fibonacci_Series
+{
+    internal class Using_While_Loop
+    {
+        static void Main()
+        {
+            Console.Write("Enter the Count : ");
+            int n = int.Parse(Console.ReadLine());
+
+            int a = 0, b = 1, c, count = 0;
+
+            Console.Write("Fibonacci series : ");
+
+            while (count <= n)
+            {
+                Console.Write(a + " ");
+                c = a + b;
+                a = b;
+                b = c;
+                count++;
+            }
+        }
+    }
+}
