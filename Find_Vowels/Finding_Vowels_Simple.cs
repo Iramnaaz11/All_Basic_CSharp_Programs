8874a752b84cda8c1eebef7fb1f68bca1a5d7273
iramnaaz11 <irapansare11@gmail.com>
2025-11-23 23:36:27 +0530

    Added a Find vowel program in simple way

---
+++ Find_Vowels/Finding_Vowels_Simple.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace All_Basic_Programs
+{
+    internal class Finding_Vowels_Simple
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine().ToLower();
+
+            string vowels = "aeiou";
+            int count = 0;
+
+            Console.Write("Vowels in your name: ");
+
+            foreach (char ch in name)
+            {
+                if (vowels.Contains(ch.ToString()))
+                {
+                    Console.Write(ch + " ");
+                    count++;
+                }
+            }
+
+            Console.WriteLine("\nTotal vowels: " + count);
+
+            Console.WriteLine();      
+            Console.ReadLine();       
+        }
+    }
+}
