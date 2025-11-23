5c945d98b67035b8cefbd623bb980d73c55cb29d
iramnaaz11 <irapansare11@gmail.com>
2025-11-23 23:42:30 +0530

    Added find vowels program using Switch

---
+++ Find_Vowels/Using_Switch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace All_Basic_Programs.Find_Vowels
+{
+    internal class Using_Switch
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine().ToLower();
+
+            int count = 0;
+
+            Console.Write("Vowels: ");
+
+            foreach (char ch in name)
+            {
+                switch (ch)
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        Console.Write(ch + " ");
+                        count++;
+                        break;
+                }
+            }
+
+            Console.WriteLine("\nTotal vowels: " + count);
+            Console.ReadLine();
+        }
+    }
+}
