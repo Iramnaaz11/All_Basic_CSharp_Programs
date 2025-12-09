5774a59c12ae9eb5db6649264388918c0d485d1c
iramnaaz11 <irapansare11@gmail.com>
2025-12-09 18:59:50 +0530

    Practise program to find number is ODD or EVEN

---
+++ Practise_Programs/Odd_Even.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace All_Basic_Programs.Practise_Programs
+{
+    internal class Odd_Even
+    {
+        static void Main()
+        {
+            int num = 10;
+            if (num % 2 == 0 )
+                Console.WriteLine("Even Number");
+                else
+                
+                    Console.WriteLine("Odd Number");
+                
+            }
+        }
+
+    }
+
