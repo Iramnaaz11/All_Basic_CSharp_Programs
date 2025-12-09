611953148b57ddbbed3d72aa8db13a9b8bafe0c5
iramnaaz11 <irapansare11@gmail.com>
2025-12-09 23:43:44 +0530

    Added a program to find fibonacci series.

---
+++ Practise_Programs/Fibo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+
+namespace All_Basic_Programs.Practise_Programs
+{
+    internal class Fibo
+    {
+        static void Main()
+        {
+            int a = 0, b = 1, c;
+            Console.WriteLine("Fibonacci Series:");
+
+            Console.Write(a + " " + b + " ");
+            for (int i = 2; i < 10; i++)
+            {
+                c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
