2e8729137024de1b5feace94e2cac6619361433a
iramnaaz11 <irapansare11@gmail.com>
2025-12-09 23:34:43 +0530

    Added a program to check entered string is Palindrome or not.

---
+++ Practise_Programs/Palindrome_string.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace All_Basic_Programs.Practise_Programs
+{
+    internal class Palindrome_string
+    {
+        static void Main()
+        {
+            string str = "madam";
+            string rev = new string(str.Reverse().ToArray());
+
+            if (str == rev)
+            {
+                Console.WriteLine("Entered string is Palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("Entered string is not a Palindrome.");
+            }
+        }
+    }
+}
