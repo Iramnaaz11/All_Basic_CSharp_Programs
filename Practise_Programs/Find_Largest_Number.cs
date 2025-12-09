7642516f79f6c7d7aa33f12d301acd7a207456d6
iramnaaz11 <irapansare11@gmail.com>
2025-12-09 19:08:30 +0530

    Program to find Largest number

---
+++ Practise_Programs/Find_Largest_Number.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace All_Basic_Programs.Practise_Programs
+{
+    internal class Find_Largest_Number
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter number 1 : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 2 : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1  > num2 )
+                Console.WriteLine("Largest number is : " + num1);
+            else
+                Console.WriteLine("Largest number is : " + num2);
+
+
+        }
+    }
+}
