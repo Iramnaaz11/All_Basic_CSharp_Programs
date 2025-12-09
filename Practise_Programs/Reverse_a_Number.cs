dfb0b8685a21c007372f68fd566ae359954f3d2b
iramnaaz11 <irapansare11@gmail.com>
2025-12-09 23:27:39 +0530

    Added a reverse number program

---
+++ Practise_Programs/Reverse_a_Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace All_Basic_Programs.Practise_Programs
+{
+    internal class Reverse_a_Number
+    {
+        static void Main()
+        {
+            {
+                int num = 3264;
+                int rev = 0;
+
+                while (num > 0)
+                {
+                    int digit = num % 10;
+                    rev = rev * 10 + digit;
+                    num = num / 10;
+                }
+                Console.WriteLine("Reversed Number: " + rev);
+            }
+        }
+    }
+}
