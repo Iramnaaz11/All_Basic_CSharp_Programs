ddf9e1019ba7ebd48b6770ce826c1f736e11b466
iramnaaz11 <irapansare11@gmail.com>
2025-12-09 23:56:34 +0530

    Added a program to count the VOWELS

---
+++ Practise_Programs/Count_Vowels.cs
@@ -0,0 +1,29 @@
+﻿using All_Basic_Programs.ReverseString;
+using System;
+using System.Linq;
+
+
+namespace All_Basic_Programs.Practise_Programs
+{
+    internal class Count_Vowels
+    {
+
+        static void Main()
+        {
+            string str = "iravira";
+            int count = 0;
+            string vowels = "aeiouAEIOU";
+
+            foreach (char c in str)
+            {
+                if (vowels.Contains(c))
+                {
+                    count++;
+                }
+
+            }
+            Console.WriteLine("Number of vowels in the string: " + count);
+
+        }
+    }    
+}
