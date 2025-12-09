f97f2152b71fcec2f5cee6242553b94a03325888
iramnaaz11 <irapansare11@gmail.com>
2025-12-09 19:28:23 +0530

    Program to Reverse a string

---
+++ Practise_Programs/Reverse_a_String.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace All_Basic_Programs.Practise_Programs
+{
+    internal class Reverse_a_String
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a string to reverse: ");
+            string input = Console.ReadLine();
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedString = new string(charArray);
+            Console.WriteLine("Reversed string: " + reversedString);
+        }
+    }
+}
+
+//Another Method to Reverse a String : 
+
+/*{
+string str = "Hello";
+char[] arr = str.ToCharArray();
+Array.Reverse(arr);
+Console.WriteLine(new string(arr));
+}*/
