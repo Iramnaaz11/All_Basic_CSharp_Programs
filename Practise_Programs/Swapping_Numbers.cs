a02ffec92dc0b2f71b820dfc26f55e2be6011de8
iramnaaz11 <irapansare11@gmail.com>
2025-12-10 23:28:59 +0530

    Added a program swapping the numbers without using third variable.

---
+++ Practise_Programs/Swapping_Numbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_Basic_Programs.Practise_Programs
+{
+    internal class Swapping_Numbers
+    {
+        static void Main()
+        {
+            int a = 10, b = 20;
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("a = " + a + ", b = " + b);
+        }
+    }
+}
