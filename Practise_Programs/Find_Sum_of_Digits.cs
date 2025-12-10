91c629c3c0c62e56a2a95931d3d7d6b51f6b4a11
iramnaaz11 <irapansare11@gmail.com>
2025-12-10 23:26:08 +0530

    Added  a program to find sum of digits

---
+++ Practise_Programs/Find_Sum_of_Digits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_Basic_Programs.Practise_Programs
+{
+    internal class Find_Sum_of_Digits
+    {
+        static void Main()
+        {
+            int num = 1234;
+            int sum = 0;
+
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            Console.WriteLine("Sum: " + sum);
+        }
+    }
+}
