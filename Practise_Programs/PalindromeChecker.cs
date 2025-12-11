9cc40ea65bb5ba518d7e747038622583e45fb55e
iramnaaz11 <irapansare11@gmail.com>
2025-12-11 22:47:39 +0530

    Added a program to check if entered string is palindrome or not.

---
+++ Practise_Programs/PalindromeChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_Basic_Programs.Practise_Programs
+{
+    internal class PalindromeChecker
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a string : ");
+            string str = Console.ReadLine();
+
+            string rev = new string(str.Reverse().ToArray());
+            Console.WriteLine(rev);
+
+            if (str == rev)
+            {
+                Console.WriteLine("Entered string is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Entered string is not a Palindrome");
+            }
+        }
+    }
+}
